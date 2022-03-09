6956e08a0c62d623013c052d6a78e65148229163
DJVERTER <creativekira1982@gmail.com>
2022-03-09 21:27:18 +0200

    cleanup code

--- PatternStrategy/Herro.cs
+++ PatternStrategy/Herro.cs
@@ -1,9 +1,5 @@
 ﻿using PatternStrategy.Strategies;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PatternStrategy
 {
--- PatternStrategy/Program.cs
+++ PatternStrategy/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using PatternStrategy;
+﻿using PatternStrategy;
 using PatternStrategy.Strategies;
+using System;
 
 Herro hero = new("Squidward");
 
